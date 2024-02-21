0d80bd198a92fa3ee5a3bed09aa88ca721635b1b
Dario <darionuberu@gmail.com>
2024-02-21 18:37:18 +0100

    test: fasñldkfjalñskdjf

--- CSharp/Parrot.Tests/ParrotTest.cs
+++ CSharp/Parrot.Tests/ParrotTest.cs
@@ -28,42 +28,42 @@ namespace Parrot.Tests
         [Fact]
         public void GetSpeedOfAfricanParrot_With_No_Coconuts()
         {
-            var parrot = new Parrot(ParrotTypeEnum.AFRICAN, 0, 0, false);
+            var parrot = new AfricanParrot(ParrotTypeEnum.AFRICAN, 0, 0, false);
             Assert.Equal(12.0, parrot.GetSpeed());
         }
 
         [Fact]
         public void GetSpeedOfAfricanParrot_With_One_Coconut()
         {
-            var parrot = new Parrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
+            var parrot = new AfricanParrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
             Assert.Equal(3.0, parrot.GetSpeed());
         }
 
         [Fact]
         public void GetSpeedOfAfricanParrot_With_Two_Coconuts()
         {
-            var parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
+            var parrot = new AfricanParrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
             Assert.Equal(0.0, parrot.GetSpeed());
         }
 
         [Fact]
         public void GetSpeedOfEuropeanParrot()
         {
-            var parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
+            var parrot = new EuropeanParrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
             Assert.Equal(12.0, parrot.GetSpeed());
         }
         
         [Fact]
         public void GetCryOfEuropeanParrot()
         {
-            var parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
+            var parrot = new EuropeanParrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
             Assert.Equal("Sqoork!", parrot.GetCry());
         }
         
         [Fact]
         public void GetCryOfAfricanParrot()
         {
-            var parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
+            var parrot = new AfricanParrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
             Assert.Equal("Sqaark!", parrot.GetCry());
         }
         
