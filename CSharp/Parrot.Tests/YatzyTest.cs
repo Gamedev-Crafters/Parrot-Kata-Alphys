aceceb337c2e6b0e3bbda72ddc134fd2f2e33fd4
Javierdds <deduenassantanojavier@gmail.com>
2024-03-15 17:01:30 +0100

    docs: requisitos yatzy

---
+++ CSharp/Parrot.Tests/YatzyTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Parrot.Tests;
+
+/*
+ * El juego se conforma de un número de rondas en el que se asocia
+ * una tirada con una categoría.
+ * 
+ * 1. El jugador tira 5 dados de 6 caras
+ * 2. Se pueden volver a realizar las tiradas hasta 2 veces
+ *  2.1 Se pueden volver a tirar cualquier número de dados
+ * 3. La tirada puede clasificarse en una categoría
+ *  3.1 La categoría no puede repetirse en diferentes rondas
+ * 4. Se suman los puntos obtenidos en cada ronda, que dependen
+ *    de la asociación de la tirada con la categoría.
+ */
+
+public class YatzyTest
+{
+        
+}
